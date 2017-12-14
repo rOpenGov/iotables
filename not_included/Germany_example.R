385f6f8d5e143a594de9194ff0a5abab800d1494
antaldaniel <antaldaniel@gmail.com>
2017-12-14 23:22:17 +0100

    German example with new functions for testing.

--- not_included/Germany_example.R
+++ not_included/Germany_example.R
@@ -1,18 +1,48 @@
-library ( iotables)
-data (germany_1990)
+library (iotables) ; library (dplyr) ; library (devtools)
+load_all()
+source = "germany_1990"
+geo <- geo_input <- "DE"
+year = 1990
+unit <- unit_input <- "MIO_EUR"
+households = FALSE
+labelling = "iotables"
 de_use <- use_table_get ( source = "germany_1990", geo = "DE",
-                                         year = 1990, unit = "MIO_EUR", 
-                                         households = FALSE, labelling = "iotables")
+           year = 1990, unit = "MIO_EUR", 
+           households = FALSE, labelling = "iotables")
 
 de_output <- output_get ( source = "germany_1990", geo = "DE",
-                                    year = 1990, unit = "MIO_EUR",
-                                    households = FALSE, labelling = "iotables")
+            year = 1990, unit = "MIO_EUR",
+            households = FALSE, labelling = "iotables")
 
-de_coeff <- input_coefficient_matrix_create( de_use, de_output, digits = 4)
-
-value_added_de <- input_value_added_get( labelled_io_data = germany_1990, 
-      technology = NULL, geo = "DE", 
-      year = 1990, unit = "M_EUR", named = TRUE) 
-
-L_de <- leontieff_matrix_create(input_coefficients_de)
+L_de <- leontieff_matrix_create(de_coeff)
 I_de <- leontieff_inverse_create(L_de)
+
+de_emp <- primary_input_get ( input = "employment_total",
+                              source = "germany_1990", geo = "DE",
+                              year = 1990,  
+                              households = FALSE, labelling = "iotables")
+
+de_emp_input <- input_indicator_create(de_emp, de_output)
+multiplier_create ( de_emp_input, I_de, "hello", digits = 4)
+round(as.matrix(de_emp_input[,2:7]) %*% as.matrix(I_de[,2:7]),4)
+
+
+
+de_emissions <- readxl::read_excel(path = 
+      "not_included/germany_emissions.xlsx")
+de_demand <-  t(de_output[2:ncol(de_output)]) - rowSums(de_use[,2:ncol(de_use)]) 
+diag_demand <- diag ( as.vector(t(de_demand)), nrow(de_demand), nrow(de_demand) )
+de_coeff <- input_coefficient_matrix_create( de_use, de_output, digits = 4)
+de_emp_coeff <- input_coefficient_matrix_create( de_use, de_emp, digits = 4)
+bi <- as.matrix(de_emp_coeff[,2:ncol(de_emp_coeff)]) %*% as.matrix(I_de [,2:ncol(I_de)])
+
+employment_coefficients <- de_emp
+for (i in 2:ncol(employment_coefficients )) {
+  employment_coefficients [,i] <- (employment_coefficients [,i]  / as.numeric(de_output[i]) )}
+
+as.matrix(emission_coefficients[,2:7])%*% as.matrix(I_de[,2:7])%*%diag(ger$output_bp[1:6] - rowSums(ger[1:6,2:7]))
+
+as.matrix(employment_coefficients[,2:7]) %*% as.matrix(I_de[,2:7])
+
+
+
