ff8e50a70c61efa1b524adc9c17d3623b14ebb12
antaldaniel <antaldaniel@gmail.com>
2018-07-16 12:54:28 +0200

    still the real_estate_services_b issue

--- data-raw/create_metadata.R
+++ data-raw/create_metadata.R
@@ -1,6 +1,6 @@
 
 library (dplyr)
-metadata <- readxl::read_excel("C:/Users/Daniel Antal/OneDrive - Visegrad Investments/2017 Projektek/iotables/data-raw/metadata.xlsx", 
+metadata <- readxl::read_excel("data-raw/metadata.xlsx", 
                                sheet = "all") %>%
   dplyr::arrange( numeric_label )
 
