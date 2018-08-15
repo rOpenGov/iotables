8f99889f1d64a56dbe87dcbb33454944a6877c09
antaldaniel <antaldaniel@gmail.com>
2018-08-15 19:43:58 +0200

    pipe

---
+++ R/pipe_operator.R
@@ -0,0 +1,10 @@
+#' Pipe operator
+#'
+#' @importFrom magrittr %>%
+#' @name %>%
+#' @rdname pipe
+#' @export
+#' @param lhs,rhs A visualisation and a function to apply to it
+#' @examples
+#' mtcars %>% summary
+NULL
