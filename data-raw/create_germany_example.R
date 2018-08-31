ee76806fa7a06d7c04f9823e9a95fc823c5a2450
antaldaniel <antaldaniel@gmail.com>
2018-08-31 20:05:55 +0200

    Reference in README.Rmd

--- data-raw/create_germany_example.R
+++ data-raw/create_germany_example.R
@@ -2,8 +2,10 @@ library (dplyr) ; library(tidyr) ; library (devtools)
 
 germany <- read.csv ( "data-raw/Beutel_15_4.csv", stringsAsFactors = F)
 
+names ( germany )
+
 germany_long <- germany %>%
-  gather  ( t_cols2, values, agriculture_group:output_bp) %>%
+  gather ( t_cols2, values, agriculture_group:output_bp) %>%
   mutate ( t_cols2_lab  = plyr::mapvalues (t_cols2, 
                                            from = c('agriculture_group', 'manufacturing_group', 'construcion_group',
                                                     'trade_group', 'business_services_group', 'other_services_group', 
@@ -15,6 +17,16 @@ germany_long <- germany %>%
                                                   "Government consumption", "Gross capital formation", "Inventory change", 
                                                   "Exports", "Output at basic prices"))) %>%
   mutate ( iotables_label_c = t_cols2 ) %>%
+  mutate ( ordering_r  = plyr::mapvalues (t_rows2, 
+                                          from = c('cpa_a', 'cpa_c', 'cpa_f',
+                                                   'cpa_g_i', 'cpa_business', 
+                                                   'cpa_other', 'cpa_total', 'P7',
+                                                   'D21_M_D31', 'P2PP', 'D1',
+                                                   'D29_M_D39', 'K1', 'B2N_B3N',
+                                                   'B1G', 'P1', 
+                                                   'EMP-WS', 'EMP-FTE', 'EMP'  
+                                                   ), 
+                                          to = seq(1:19))) %>%
   mutate ( ordering_c  = plyr::mapvalues (t_cols2, 
                                           from = c('agriculture_group', 'manufacturing_group', 'construcion_group',
                                                    'trade_group', 'business_services_group', 'other_services_group', 
@@ -22,7 +34,8 @@ germany_long <- germany %>%
                                                    'consumption_expenditure_government', 'gross_capital_formation',
                                                    'inventory_change', 'export_goods_services', 'output_bp'), 
                                           to = seq(1:12))) %>%
-  mutate ( ordering_c = as.numeric(ordering_c)) %>%
+  mutate ( ordering_c = as.numeric(ordering_c), 
+           ordering_r = as.numeric(ordering_r)) %>%
   mutate ( r_quadrant = ifelse ( ordering_r > 6, yes = "2_4", no = "1_3")) %>%
   mutate ( c_quadrant = ifelse ( ordering_c > 6, yes = "3_4", no = "1_2")) %>%
   mutate ( geo  = "DE") %>%
@@ -36,9 +49,9 @@ germany_long <- germany %>%
                                           as.numeric(ordering_r))) %>%
   mutate ( t_cols2 = forcats::fct_reorder(t_cols2, 
                                           as.numeric(ordering_c))) %>%
-  mutate ( iotables_label_r = forcats::fct_reorder(iotables_label_r , 
+  mutate ( iotables_label_r = forcats::fct_reorder(t_rows2  , 
                                                    as.numeric(ordering_r))) %>%
-  mutate ( iotables_label_c = forcats::fct_reorder(iotables_label_c, 
+  mutate ( iotables_label_c = forcats::fct_reorder(t_cols2, 
                                                    as.numeric(ordering_c))) %>%
   arrange ( t_rows2, t_cols2 )
 
