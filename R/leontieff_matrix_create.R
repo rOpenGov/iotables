4c65bf571905b5dc37859d92e8d55e4e583e59fd
antaldaniel <antaldaniel@gmail.com>
2018-09-10 20:23:29 +0200

    documentation for output coefficients

--- R/leontieff_matrix_create.R
+++ R/leontieff_matrix_create.R
@@ -3,7 +3,9 @@
 #' Create a Leontieff matrix from technology matrix after some basic error 
 #' handling. Most likely you will need this function as a step to invoke the 
 #' function to create its inverse: \code{\link{leontieff_inverse_create}}.
-#' @param technology_coefficients_matrix A technology coefficient matrix created by the \code{\link{input_coefficient_matrix_create}}.
+#' @param technology_coefficients_matrix A technology coefficient matrix created
+#' by the \code{\link{input_coefficient_matrix_create}} or 
+#' \code{\link{output_coefficient_matrix_create}}.
 #' @importFrom dplyr mutate_at mutate_if
 #' @examples 
 #' de_use <- use_table_get ( source = "germany_1990", geo = "DE",
