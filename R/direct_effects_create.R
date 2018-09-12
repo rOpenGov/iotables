b7cf1e3c4987cacc7d0ccb61c6014889890488bb
antaldaniel <antaldaniel@gmail.com>
2018-09-12 15:44:10 +0200

    removing all zero columns

--- R/direct_effects_create.R
+++ R/direct_effects_create.R
@@ -25,12 +25,9 @@ direct_effects_create <- function ( labelled_io_table,
   if (! is.null(digits)) {
     if (digits<0) digits <- NULL
   }
-  
-  total_row_number <- which (tolower(as.character(unlist(labelled_io_table[,1]))) %in% c("total", "cpa_total"))
-  total_column_number <- which ( tolower(names(labelled_io_table)) %in% c("cpa_total", "total"))
-  
+
   if (type == "intermediate") {
-    demand_row_number <- total_row_number
+    demand_row_number <- which (tolower(as.character(unlist(labelled_io_table[,1]))) %in% c("total", "cpa_total"))
   } else if (type == "domestic_demand")  {
     demand_row_number <- which ( tolower(
       as.character(unlist(labelled_io_table[,1]))) %in% c(
@@ -46,6 +43,44 @@ direct_effects_create <- function ( labelled_io_table,
   if ( length (demand_row_number) == 0 ) {
     stop("Demand row was not found.")
   }
+  
+  
+  ###Removing all zero columns and rows --------
+  
+  #Determine if a column is all zero
+  non_zero <- function (x) {
+    if ( class ( x ) %in% c("factor", "character") ) return ( TRUE )
+    ifelse (  sum ( as.numeric ( unlist (x) ), na.rm=TRUE) == 0, FALSE, TRUE )
+  }
+  
+  #Examine which columns are filled with zeros
+  non_zero_cols <- vapply ( labelled_io_table, 
+                            non_zero, logical (1) )
+  non_zero_rows <- labelled_io_table[-1] 
+  
+  #Remove columns that are filled with zeros
+  remove_cols <- names (labelled_io_table)[! non_zero_cols ]
+  
+  if ( length( remove_cols) > 0 ) {
+    warning ("Columns ", paste(remove_cols, collapse =', '), " are all zeros and will be removed.")
+  }
+  
+  siot_rows <- as.character ( unlist ( labelled_io_table[,1]) )
+  siot_rows
+  #names ( input_flow) [! names ( input_flow ) %in% remove_cols ]
+  # siot_rows [! siot_rows %in% remove_cols ]
+  
+  ##Now remove all zero corresponding rows
+  labelled_io_table<- labelled_io_table [! siot_rows %in% remove_cols , 
+                            ! names ( labelled_io_table) %in% remove_cols  ]
+  
+  labelled_io_table <- dplyr::mutate_if ( labelled_io_table, 
+                                          is.factor, as.character )
+  
+  ###Separate io table quadrants------
+  total_row_number <- which (tolower(as.character(unlist(labelled_io_table[,1]))) %in% c("total", "cpa_total"))
+  total_column_number <- which ( tolower(names(labelled_io_table)) %in% c("cpa_total", "total"))
+  
  
   lq_rows <- total_row_number:nrow(labelled_io_table)
   lower_quadrant <- labelled_io_table [ lq_rows, 1:ncol(labelled_io_table) ]
@@ -63,23 +98,24 @@ direct_effects_create <- function ( labelled_io_table,
     }
   
   demand_row <- labelled_io_table[demand_row_number, 1:total_column_number ]
-  
-  not_na_cols <- which (! is.na(as.numeric(demand_row)))
+  not_na_cols <- which (! is.na(as.numeric(demand_row[ 2:length(demand_row)])))
 
   if (any(! not_na_cols)) {
     warning ( "The columns ", 
               paste(names(labelled_io_table)[!not_na_cols], collapse = ', '), 
-              "were removed due to missing data.")
+              "were removed due to missing demand data.")
   }
   
+  #remove columns where demand is not known
   demand_row <- demand_row [, not_na_cols]
   lower_quadrant <- lower_quadrant [, not_na_cols]
   
-  zero_totals <- which ( as.numeric(demand_row) ==0 )
+  zero_totals <- which ( as.numeric(demand_row[ 2:length(demand_row)]) ==0 )
   if ( length(zero_totals > 0) ) {
     for ( j in zero_totals ) {
       demand_row[, j] <- 0.0000001
-      warning ( "Zero total values were changed to epsilon do avoid division by zero")
+      warning ( "Zero total value ", 
+      names ( demand_row )[j], " was changed to to epsilon do avoid division by zero")
     }
   } #end of replacing zeros 
 
@@ -91,7 +127,7 @@ direct_effects_create <- function ( labelled_io_table,
     if ( class(digits) %in% c("numeric", "integer")) {
       stop("Digits must be a number.")
     }
-    lower_quadrant <- round(lower_quadrant[, 2:ncol(lower_quadrant)], digits)
+    lower_quadrant <- round(lower_quadrant[, 2:ncol(lower_quadrant)], digits) #actual round
     }
   lower_quadrant
 }
