5b05065c243e16b75d73c35a993cdd801ef7cfdb
antaldaniel <antaldaniel@gmail.com>
2018-11-29 18:24:43 +0100

    Supply table at basic prices incl. transformation into purchasers' prices

--- R/iotable_get.R
+++ R/iotable_get.R
@@ -11,6 +11,7 @@
 ##'  \item{\code{naio_10_pyp1700}}{ Symmetric input-output table at basic prices (product by product) (previous years prices)}
 ##'  \item{\code{naio_10_cp1750}}{ Symmetric input-output table at basic prices (industry by industry)}
 ##'  \item{\code{naio_10_pyp1750}}{ Symmetric input-output table at basic prices (industry by industry) (previous years prices) }
+##'  \item{\code{naio_10_cp15}}{ Supply table at basic prices incl. transformation into purchasers' prices }
 ##'  \item{\code{naio_10_cp16}}{ Use table at purchasers' prices }
 ##'  \item{\code{naio_10_cp1610}}{ Use table at basic prices }
 ##'  \item{\code{naio_10_pyp1610}}{ Use table at basic prices (previous years prices) (naio_10_pyp1610) }
@@ -76,7 +77,7 @@ iotable_get <- function ( labelled_io_data = NULL,
   }
 ##Veryfing source parameter and loading the labelling  ----
   prod_ind <- c("naio_10_cp1700", "naio_10_cp1750", "naio_10_pyp1700",
-                "naio_10_pyp1750", "naio_10_cp16",
+                "naio_10_pyp1750", "naio_10_cp15", "naio_10_cp16",
                 "naio_10_cp1610", "naio_10_cp1620", "naio_10_cp1630", 
                 "naio_10_pyp1620", "naio_10_pyp1630" )
   trow_tcol <-  c(  "croatia_2010_1700", "croatia_2010_1800", "croatia_2010_1900")
