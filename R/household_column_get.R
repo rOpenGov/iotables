ede3541f80269f3e2c77168ff9178b20275033d1
antaldaniel <antaldaniel@gmail.com>
2018-12-07 18:19:31 +0100

    @return The column containing final household expenditure. If not found

---
+++ R/household_column_get.R
@@ -0,0 +1,22 @@
+#' Return final household expenditure
+#'  
+#' @param data_table A symmetric input output table, a use table or a supply 
+#' table.
+#' @return The column containing final household expenditure. If not found
+#' \code{NULL} is returned.
+#' @importFrom dplyr mutate_at
+#' @examples 
+#' household_column_get( iotable_get ( source = 'germany_1990') )
+#' @export
+
+household_column_get <- function(data_table) {
+ 
+ household_column <- household_column_find(data_table)
+
+ fn_na_to_null <- function(x) ifelse (is.na(x), 0,x)
+ if ( ! is.null( household_column) ) {
+   data_table[, c(1, household_column)] %>%
+     dplyr::mutate_at (vars(2), fn_na_to_null)
+ } else { return(NULL) }
+ 
+}
