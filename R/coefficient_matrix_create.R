8f54f149a39c9598d42ad370b07bfc4e11804b4a
antaldaniel <antaldaniel@gmail.com>
2018-12-07 18:19:58 +0100

    households

--- R/coefficient_matrix_create.R
+++ R/coefficient_matrix_create.R
@@ -18,9 +18,11 @@
 #' @param empty_rows_remove Defaults to \code{TRUE}. If you want to keep empty 
 #' primary input rows, choose \code{FALSE}. Empty product/industry rows are always 
 #' removed to avoid division by zero error in the analytical functions.
+#' @param households Defaults to \code{NULL}. Household column can be added 
+#' with \code{TRUE}.
 #' @return A data.frame that contains the matrix of  \code{siot} devided by \code{total}
 #' with a key column. Optionally the results are rounded to given \code{digits}. 
-#' @importFrom dplyr mutate mutate_if full_join funs
+#' @importFrom dplyr mutate mutate_if full_join funs left_join
 #' @references See \href{https://webarchive.nationalarchives.gov.uk/20160114044923/http://www.ons.gov.uk/ons/rel/input-output/input-output-analytical-tables/2010/index.html}{United Kingdom Input-Output Analytical Tables 2010}
 #' for explanation on the use of the Coefficient matrix.
 #' @examples 
@@ -33,6 +35,7 @@ coefficient_matrix_create <- function ( siot,
                                         total = "output", 
                                         digits = NULL, 
                                         empty_rows_remove = TRUE,
+                                        households = FALSE,
                                         return = NULL) {
   
   #Create a coefficient matrix, including primary inputs.  
@@ -71,20 +74,27 @@ coefficient_matrix_create <- function ( siot,
                             ! names ( siot ) %in% remove_cols  ]
   siot <- dplyr::mutate_if ( siot, is.factor, as.character )
   
-  germany_90 <- iotable_get(source = 'germany_1990') #for compatibility with simplified demo
+  last_column <- quadrant_separator_find( siot )
+
   
-  if ( all( names(siot)  == names ( germany_90)) ) {
-    last_column <- which ( tolower(names(siot)) %in% c("other_services_group"))
-  } else {
-    last_column <- which ( tolower(names(siot)) %in% c("cpa_total", "total")) [1]
+  if ( !is.null(households)) {
+    if ( households == TRUE) { household_column <- household_column_get( siot )
+    quadrant <- siot [, 1:last_column]
+    siot <- dplyr::left_join ( quadrant, household_column, 
+                               by = names(quadrant)[1])
+    } else {
+      siot <- siot [, 1:last_column]
+    }
+      } else {
+    siot <- siot [, 1:last_column]
   }
   
-  if ( tolower (total)  %in% c("output", "p1", "output_bp") ) {
-    total_row <- siot[which ( tolower (unlist(siot[,1]))  %in% c("output", "p1", "output_bp"))[1], 
-                      1:last_column]
+ 
+  if ( any( c("output", "p1", "output_bp")  %in%  tolower (siot[,1])) ) {
+    total_row <- siot[which ( tolower (unlist(tolower(siot[,1])))  %in%
+                                c("output", "p1", "output_bp"))[1],]
   } else {
-    total_row <- siot[which ( tolower (unlist(siot[,1]))  %in% c(total))[1], 
-                      1:last_column]
+    total_row <- siot[which ( tolower (unlist(siot[,1]))  %in% c(total))[1],]
   }
   
     
@@ -92,17 +102,15 @@ coefficient_matrix_create <- function ( siot,
   null_to_na <- function(x) ifelse (x == 0, NA, x )
   total_row <- dplyr::mutate_if ( total_row, is.factor, as.character )
   total_row <- dplyr::mutate_if ( total_row, is.numeric, null_to_na ) #avoid division by zero
-  
-  siot <- siot[, 1:last_column]
-  
-  for (j in 2:last_column) {
+
+  for (j in 2:ncol(siot)) {
     siot[ ,j] <-  siot[ ,j] / unlist(total_row [, j])
     }
   
    
-  if ( ! is.null(return)) {
+  if ( ! is.null(return) )  {
     
-    last_row <- which ( tolower(unlist(siot[,1])) %in% tolower(c("CPA_TOTAL", "TOTAL")))
+    last_row <- which ( tolower(unlist(siot[,1])) %in% c("cpa_total", "total"))
     
     if ( return == "primary_inputs" ) {
       siot <- siot[last_row:nrow(siot), ]
@@ -113,10 +121,13 @@ coefficient_matrix_create <- function ( siot,
   
   if ( empty_rows_remove == TRUE ) { 
     
-    message ( "Removed empty rows ",
-      paste ( unlist(siot[which( rowSums(is.na(siot)) >= ncol(siot)-1),1]), 
-              collapse = ', '), '.')
-    siot <- siot[which( rowSums(is.na(siot)) != ncol(siot)-1), ]
+    if (length(which( rowSums(is.na(siot)) >= ncol(siot)-1)) > 0) {
+      message ( "Removed empty rows ",
+                paste ( unlist(siot[which( rowSums(is.na(siot)) >= ncol(siot)-1),1]), 
+                        collapse = ', '), '.')
+    }
+    
+    siot <- siot[which( rowSums(is.na(siot)) != ncol(siot)-1 ), ]
     
     }
   
