65b61978ebec243ddd7f6d3a0cf6d19f13cf7bf3
antaldaniel <antaldaniel@gmail.com>
2018-12-08 19:11:40 +0100

    very thorough rewrite

--- R/input_multipliers_create.R
+++ R/input_multipliers_create.R
@@ -4,12 +4,9 @@
 #' @param direct_effects A direct effects matrix created by 
 #' \code{\link{direct_supply_effects_create}}
 #' @param inverse A Leontieff-inverse created by \code{\link{leontieff_inverse_create}}.
-#' @param labelled Defaults to \code{TRUE}. If you use not labelled matrixes, i.e. 
-#' both inputed matrixes are numerical only without the first key row, select 
-#' \code{FALSE}.
 #' @param digits Rounding digits, defaults to \code{NULL}, in which case 
 #' no rounding takes place.  
-#' @importFrom dplyr select one_of
+#' @importFrom dplyr select one_of mutate_at
 #' @examples  
 #' de_use <- use_table_get()
 #' L_de  <- leontieff_matrix_create( de_use )
@@ -25,17 +22,22 @@
 #' 
 #' multipliers <- input_multipliers_create(
 #'       direct_effects = direct_effects_de [, -8],
-#'       inverse = I_de, 
-#'       labelled = TRUE)
+#'       inverse = I_de)
 #' @export
 
 input_multipliers_create <- function ( direct_effects,
-                                       inverse, 
-                                       labelled = TRUE,
-                                       digits = NULL ) { 
+                                       inverse,
+                                       digits = NULL, 
+                                       multiplier_names = NULL) { 
  
   
   names_direct <- names ( direct_effects )
+  if ( is.null(multiplier_names)) {
+    direct_effects <- direct_effects %>%
+      dplyr::mutate_at ( vars(1), funs(gsub(pattern ="_indicator",
+                                       replacement = "_multiplier", 
+                                       x =. )))
+  }
   
   if ( all ( names (inverse) %in% names ( direct_effects ) ) ) {
     direct_effects <- dplyr::select ( direct_effects, 
@@ -47,41 +49,29 @@ input_multipliers_create <- function ( direct_effects,
     stop("The direct effects matrix and the Leontieff inverse must have the same number of columns.")
   }
   
-  #columns of the left matrix must be the same as the number of rows of the right matrix
+  #columns of the left matrix must be the same as the number of rows of 
+  #the right matrix
+  #Remove key column------
+  first_column   <- subset ( direct_effects, select = 1)
+  direct_effects <- direct_effects[,-1]
+  inverse        <- inverse[, -1]
 
-  if ( labelled == TRUE) { 
-    first_column <- subset ( direct_effects, select = 1)
-    direct_effects <- direct_effects[,-1]
-    inverse <- inverse[, -1]
-  } 
-  
-  inverse <- as.matrix ( inverse )
+  inverse        <- as.matrix ( inverse )
   direct_effects <- as.matrix (direct_effects)
   
   if (ncol ( direct_effects) != nrow ( inverse ) ) {
-    stop ( "The columns of 'direct_effects' must equal the rows of the 'inverse', 
-           they must be both labelled or not labelled.")
+    stop ( "The matrix is not symmetric") #review later
   }
   
   multipliers <- direct_effects %*% inverse 
   
- if ( labelled == TRUE ) { 
-    multipliers <-  cbind (first_column, as.data.frame(multipliers))
-    if ( !is.null(digits) ) {
-      if (! class(digits) %in% c("numeric", "integer")) {
-        stop("Digits must be a number.")
-      }
-      multipliers[, 2:ncol(multipliers)] <- round(
-        multipliers[, 2:ncol(multipliers)], digits)
+  multipliers <-  cbind (first_column, as.data.frame(multipliers))
+  if ( !is.null(digits) ) {
+    if (! class(digits) %in% c("numeric", "integer")) {
+      stop("Digits must be a number.")
     }
-    } else {
-   if ( !is.null(digits) ) {
-     if (! class(digits) %in% c("numeric", "integer")) {
-       stop("Digits must be a number.")
-     }
-     multipliers[, 1:ncol(multipliers)] <- round(
-       multipliers[, 1:ncol(multipliers)], digits)
-     }
+    multipliers[, 2:ncol(multipliers)] <- round(
+      multipliers[, 2:ncol(multipliers)], digits)
   }
   
   as.data.frame ( multipliers )
