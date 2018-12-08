2a327c37acae1e5dabcc8067a6212d70cb51a9e6
antaldaniel <antaldaniel@gmail.com>
2018-12-08 17:26:41 +0100

    updated tests for new function

--- tests/testthat/test_primary_input_get.R
+++ tests/testthat/test_primary_input_get.R
@@ -2,33 +2,14 @@ library (testthat)
 library (iotables)
 context ("Retreieving a primary input vector")
 
-test_that("primary_input_get errors ", {
-  expect_error(primary_input_get(input = "b2n_b3n", source = "germany_1990", 
-                              geo = 'DE', year = 1990, unit = "MIO_NAC")) 
-  expect_error(primary_input_get(input = "D1", source = "germany_1990", 
-                                 geo = 'DE', year = 1990, unit = "MIO_NAC")) #case sensitive
-  expect_error(primary_input_get(input = "b2n_b3n", source = "germany_1990", 
-                              geo = 'DE', year = 1787, unit = "MIO_EUR")) 
-  expect_error(primary_input_get(input = "b2n_b3n", source = "germany_1990", 
-                              geo = 'BE', year = 1990, unit = "MIO_EUR"))
-  expect_error(primary_input_get(input = "b2n_b3n", source = "germany_1990", 
-                           geo = 'DE', year = 1990, 
-                           unit = "MIO_EUR", labelling = "biotables"))
-  expect_error(primary_input_get(input = "false_input", source = "germany_1990", 
-                                 geo = 'DE', year = 1990, 
-                                 unit = "MIO_EUR", labelling = "MIO_EUR"))
-})
 
 test_that("correct data is returned", {
-  expect_equal(as.numeric(primary_input_get(input = "d1", source = "germany_1990", 
-                                 geo = 'DE', year = 1990, 
-                                 unit = "MIO_EUR", households = TRUE, 
-                                 labelling = "short")[1,3]),296464 )
-  expect_equal(as.numeric(primary_input_get(input = "compensation_employees", source = "germany_1990", 
-                                 geo = 'DE', year = 1990, 
-                                 unit = "MIO_EUR", households = TRUE, 
-                                 labelling = "iotables")[1,3]),296464 ) #alternate labelling
-  })
+  expect_equal(primary_input_get(siot = iotable_get ( source = "germany_1990", 
+                                                      geo = 'DE', year = 1990, 
+                                                      unit = "MIO_EUR",
+                                                      labelling = "short"),
+                                 primary_input = "d1")[,3], 296464 )
+})
 
 
 
