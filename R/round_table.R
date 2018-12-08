cadb346c02d3c759d1e9a74f8f91de453d519136
antaldaniel <antaldaniel@gmail.com>
2018-12-08 22:46:32 +0100

    inital commit

---
+++ R/round_table.R
@@ -0,0 +1,28 @@
+#' Systematically round values in a table.
+#' 
+#' This is an internal function to do the rounding on all numeric elements of
+#' the table.
+#' @param data_table A symmetric input-output table, a use table, a supply table,
+#' a margin or tax table.
+#' @param digits 
+#' @return A tibble/data.frame with a key row and a symmetric matix, 
+#' after removing all empty columns and rows at the same time. 
+#' @examples 
+#' round_table( iotable_get ( source = "germany_1990") )
+
+round_table <- function ( data_table, 
+                          digits = NULL) {
+  
+  if (!is.null(digits)) { ##rounding digits must be numeric, if given
+    if ( class(digits) != "numeric") {
+      warning ("Error: rounding digits are not given as a numeric input, 
+               no rounding took place.") 
+      return(data_table) }
+  }
+   
+  round_eps <- function ( x, digits ) {
+    ifelse ( x == 1e-06, x, round ( x, digits ))
+  }
+  data_table %>%
+    dplyr::mutate_if(is.numeric, dplyr::funs(round_eps (., digits)))
+}
