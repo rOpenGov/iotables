4305163b5f06b73adbc2643b58f1e4b1f31ed4d0
antaldaniel <antaldaniel@gmail.com>
2018-12-08 17:26:09 +0100

    check_digit helper function

---
+++ R/check_digits.R
@@ -0,0 +1,16 @@
+#' Check digits parameter
+#' 
+#' This is an internal function to determine if the rounding can go ahead.
+#' @param data_table A symmetric input output table, a use table or a supply 
+#' table.
+#' @return An error if the digits are not \code{NULL} or an integer value.
+#' @examples 
+#' check_digits (1)
+
+check_digits <- function(digits) {
+  if (!is.null(digits)) { ##rounding digits must be numeric, if given
+    if ( class(digits) != "numeric") {
+      stop ("Error: rounding digits are not given as a numeric input.") }
+  }
+  
+}
