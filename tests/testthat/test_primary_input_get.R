669b4a2937aa5acbcc50a590f4be00fc659abe58
antaldaniel <antaldaniel@gmail.com>
2018-12-09 20:48:57 +0100

    tests are correctly running

--- tests/testthat/test_primary_input_get.R
+++ tests/testthat/test_primary_input_get.R
@@ -2,14 +2,16 @@ library (testthat)
 library (iotables)
 context ("Retreieving a primary input vector")
 
+siot <- iotable_get ( source = "germany_1990", 
+                      geo = 'DE', year = 1990, 
+                      unit = "MIO_EUR",
+                      labelling = "short")
 
 test_that("correct data is returned", {
-  expect_equal(primary_input_get(siot = iotable_get ( source = "germany_1990", 
-                                                      geo = 'DE', year = 1990, 
-                                                      unit = "MIO_EUR",
-                                                      labelling = "short"),
-                                 primary_input = "d1")[,3], 296464 )
+  expect_equal(primary_input_get(data_table = siot,
+                                 primary_input = "D1")[,3], 296464 )
 })
+
 
 
 
