53020dbab5db47c9b9d5f83d42c21c472ae8aa7f
antaldaniel <antaldaniel@gmail.com>
2018-12-09 20:38:02 +0100

    include total

--- R/quadrant_separator_find.R
+++ R/quadrant_separator_find.R
@@ -4,16 +4,20 @@
 #' necessary.
 #' @param data_table A symmetric input output table, a use table or a supply 
 #' table.
+#' @param include_total Should the total (intermediary) output column be
+#' included \code{TRUE} or excluded (\code{FALSE}, default)?
 #' @return An integer value with the last column of Quadrant I and III. If
 #' the last column is not found, \code{2} is returned with a warning to avoid
 #' stopping a pipeline.
 #' @examples 
 #' quadrant_separator_find( iotable_get ( source = "germany_1990") )
 
-quadrant_separator_find <- function(data_table) {
+quadrant_separator_find <- function(data_table, 
+                                    include_total = FALSE) {
   last_column <- 2
    if ( any(c("total", "cpa_total") %in% tolower(names(data_table))) ) {
        last_column <- which(tolower(names(data_table))  %in% c("total", "cpa_total") )
+       if ( include_total ) last_column <- last_column -1 #if total columns are not needed, the last but total
      } else if ( any(c("households", "p13_s14") %in% tolower(names(data_table)))) {
        last_column <- which(tolower(names(data_table)) %in% c("households", "p13_s14")-1 )
      } else  if ( 'cpa_u' %in% tolower ( names (data_table)) ) { 
