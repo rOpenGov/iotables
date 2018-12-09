3f2f120eaa5af125ad2dd44f22acfa67822e39ed
antaldaniel <antaldaniel@gmail.com>
2018-12-09 20:54:59 +0100

    test is successful

--- tests/testthat/test_input_indicator_create.R
+++ tests/testthat/test_input_indicator_create.R
@@ -11,6 +11,19 @@ test_that("correct data is returned", {
                expected = 0.21, tolerance = .004)
 })
 
-##needs to be checked again with better examples. 
+de_gva <- primary_input_get ( data_table,
+                              primary_input = "gva") 
+
+de_gva_indicator  <- input_indicator_create( 
+          data_table  = data_table,
+          input_vector = "gva", 
+          digits = 4)
+
+test_that("correct data is returned", {
+  expect_equal(as.numeric(de_gva_indicator[2:7]), 
+               expected = c(0.4934, 0.3659, 0.4708, 0.5766, 0.5999, 0.7172),
+               tolerance = .004)
+})
+#Eurostat manual p498
 
 
