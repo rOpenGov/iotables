4b1cec5b321fab6ec82b4a74e06cfb986adb9c23
antaldaniel <antaldaniel@gmail.com>
2018-12-10 12:35:06 +0100

    interface change. some error remains

--- tests/testthat/test_effects_create.R
+++ tests/testthat/test_effects_create.R
@@ -2,27 +2,17 @@ library (testthat)
 library (iotables)
 context ("Retreieving a primary input vector")
 
-de_use <- use_table_get ( source = "germany_1990", geo = "DE",
-                          year = 1990, unit = "MIO_EUR", 
-                          households = FALSE, labelling = "iotables")
-
-de_output <- output_get ( source = "germany_1990", geo = "DE",
-                          year = 1990, unit = "MIO_EUR",
-                          households = FALSE, labelling = "iotables")
-
-de_coeff <- input_coefficient_matrix_create( input_flow = de_use,
-                                             output = de_output, 
+de_coeff <- input_coefficient_matrix_create( iotable_get(), 
                                              digits = 4 )
 L_de <- leontieff_matrix_create( technology_coefficients_matrix =
                                    de_coeff )
-I_de <- leontieff_inverse_create(L_de)
+I_de <- leontieff_inverse_create(technology_coefficients_matrix =
+                                   de_coeff)
 
-de_emp <- primary_input_get ( input = "employment_total",
-                              source = "germany_1990", geo = "DE",
-                              year = 1990,  
-                              households = FALSE, labelling = "iotables")
+de_emp <- primary_input_get ( data_table = iotable_get(),
+                              primary_input = "employment_domestic_total" )
 
-de_emp_indicator <- input_indicator_create (de_emp, de_output, digits = 5)
+de_emp_indicator <- input_indicator_create (iotable_get(), "employment_domestic_total" , digits = 5)
 
 employment_effects <- effects_create ( 
   input_vector    = de_emp_indicator,
@@ -35,12 +25,11 @@ test_that("correct data is returned", {
   expect_equal( employment_effects$agriculture_group ,0.0326 )
   })
 
-nl_use_2 <- use_table_get  ( labelled_io_table = netherlands_2006,
-                             source = 'custom', households = TRUE)
-nl_output_2 <- output_get ( labelled_io_table = netherlands_2006, households = TRUE )
+nl_use_2 <- input_flow_get  ( netherlands_2006,
+                              households = TRUE)
 
-nl_coeff_2   <- input_coefficient_matrix_create( input_flow = nl_use_2,
-                                                 output = nl_output_2, 
+nl_coeff_2   <- input_coefficient_matrix_create( netherlands_2006,
+                                                 households = TRUE, 
                                                  digits = NULL)
 
 nl_coeff_2$final_consumption_households <- c(0, 0, 0.08, 0.03, 0, 0.59, 0) #for the time being
@@ -48,19 +37,17 @@ nl_coeff_2$final_consumption_households <- c(0, 0, 0.08, 0.03, 0, 0.59, 0) #for 
 
 L_nl_2 <- leontieff_matrix_create( technology_coefficients_matrix =
                                      nl_coeff_2 )
-I_nl_2 <- leontieff_inverse_create(L_nl_2)
+I_nl_2 <- leontieff_inverse_create(technology_coefficients_matrix =
+                                     nl_coeff_2)
 
-households <- data.frame ( 
-  final_consumption_households = 0)
 
-gva_nl_2 <- cbind(nl[12,1:7], households)
-income_nl_2 <- cbind(nl[11,1:7], households)
+gva_indicator_nl_2   <- input_indicator_create(netherlands_2006, 
+                                               "value_added_bp", 
+                                               households = TRUE)
 
-gva_indicator_nl_2   <- input_indicator_create( input_matrix = gva_nl_2, 
-                                                output_vector = nl_output_2)
-
-income_indicator_nl_2   <- input_indicator_create( input_matrix = income_nl_2, 
-                                                   output_vector = nl_output_2)
+income_indicator_nl_2 <- input_indicator_create(netherlands_2006, 
+                                                "compensation_employees", 
+                                                households = TRUE)
 
 income_nl_effects_2 <- effects_create ( 
   input_vector = income_indicator_nl_2,
