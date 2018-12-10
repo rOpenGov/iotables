efc0b1b074f8dbbf7b108fdb6406518682bab751
antaldaniel <antaldaniel@gmail.com>
2018-12-10 12:26:17 +0100

    more tests needed

--- tests/testthat/test_output_get.R
+++ tests/testthat/test_output_get.R
@@ -2,52 +2,10 @@ library (testthat)
 library (iotables)
 context ("Creating an output vector")
 
-output_get(source = "germany_1990", 
-           geo = 'DE', year = 1990, unit = "MIO_EUR")
-
-test_that("output_get errors ", {
-  expect_error(output_get(source = "germany_1990", 
-                              geo = 'DE', year = 1990, unit = "MIO_NAC")) 
-  expect_error(output_get(source = "germany_1990", 
-                              geo = 'DE', year = 1787, unit = "MIO_EUR")) 
-  expect_error(output_get(source = "germany_1990", 
-                              geo = 'BE', year = 1990, unit = "MIO_EUR"))
-  #expect_warning(use_table_get(source = "germany_1990", 
-  #                           geo = 'de', year = 1990, unit = "MIO_EUR"))
-  expect_error(output_get(source = "germany_1990", 
-                           geo = 'DE', year = 1990, 
-                           unit = "MIO_EUR", labelling = "biotables"))
-  expect_error(output_get(source = "croatia_2010_1900", 
-                          geo = 'HR', year = 2010, unit = "T_NAC"))  #this is an import table
-})
+output_vector <- output_get(iotable_get())
 
 test_that("correct data is returned", {
-  expect_equal(as.character(output_get(source = "germany_1990", 
-                          geo = 'DE', year = 1990, 
-                          unit = "MIO_EUR", 
-                          labelling = "iotables")[[1]][1]), "output_bp")
-  expect_equal(as.numeric(output_get(source = "germany_1990", 
-                 geo = 'DE', year = 1990, 
-                 unit = "MIO_EUR", households = TRUE, 
-                 labelling = "iotables")$consumption_expenditure_household), 1001060)
+  expect_equal(as.character(output_get(iotable_get())[[1]][1]), "output")
   })
 
-test_that("households are included or excluded", {
-  expect_equal("consumption_expenditure_household" %in% 
-                 names ( output_get(source = "germany_1990", 
-                                    geo = 'DE', year = 1990, 
-                                    unit = "MIO_EUR", households = FALSE, 
-                                    labelling = "iotables") ), FALSE)
-  expect_equal("consumption_expenditure_household" %in% 
-                 names ( output_get(source = "germany_1990", 
-                                    geo = 'DE', year = 1990, 
-                                    unit = "MIO_EUR", households = TRUE, 
-                                    labelling = "iotables") ), TRUE)
-  expect_equal("consumption_expenditure_government" %in% 
-               names ( output_get(source = "germany_1990", 
-                                  geo = 'DE', year = 1990, 
-                                  unit = "MIO_EUR", households = TRUE, 
-                                  labelling = "iotables") ), FALSE)
-})
-
-
+#more tests needed
