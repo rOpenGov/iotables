763ee2fd923abcc89623bcf714e4326329549839
antaldaniel <antaldaniel@gmail.com>
2018-12-11 11:17:31 +0100

    digits are not needed

--- data-raw/create_metadata.R
+++ data-raw/create_metadata.R
@@ -2,14 +2,16 @@
 library (dplyr)
 metadata <- readxl::read_excel("data-raw/metadata.xlsx", 
                                sheet = "all") %>%
-  dplyr::arrange( numeric_label )
+  dplyr::arrange( numeric_label ) %>%
+  dplyr::select ( -digit_1, -digit_2 )
 
 metadata_uk_2010 <- readxl::read_excel(path = file.path('data-raw', 'metadata_uk_2010.xlsx')) %>%
   dplyr::mutate ( uk_col = gsub("\\.", "-", as.character(uk_col))) %>%
   dplyr::mutate ( uk_col = gsub(" & ", "-", as.character(uk_col))) %>%
   dplyr::mutate ( uk_row = gsub("\\.", "-", as.character(uk_row))) %>%
   dplyr::mutate ( uk_row = gsub(" & ", "-", as.character(uk_row))) %>%
-  dplyr::mutate ( uk_col = trimws(uk_col, 'both'))
+  dplyr::mutate ( uk_col = trimws(uk_col, 'both')) %>%
+  dplyr::select ( -digit_1, -digit_2, -digit_3_5 )
 
 
 
