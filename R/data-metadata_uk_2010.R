13ccaa0e1949f6c90c6ef11e1a0febca2d7eadc0
antaldaniel <antaldaniel@gmail.com>
2018-12-11 12:46:48 +0100

    correcting bad markups

--- R/data-metadata_uk_2010.R
+++ R/data-metadata_uk_2010.R
@@ -5,10 +5,10 @@
 #' @format A data frame with 10 variables.
 #'\describe{
 #'   \item{variable}{Eurostat vocabulary source, i.e. t_rows, t_cols, prod_na, induse}
-#'   \item{uk_row} {The UK row identifier. Dots and '&' converted to '-'.} 
-#'   \item{uk_row_label} {The original UK row labels.}
+#'   \item{uk_row}{The UK row identifier. Dots and '&' converted to '-'.} 
+#'   \item{uk_row_label}{The original UK row labels.}
 #'   \item{uk_col}{The UK row identifier. Dots and '&' converted to '-'.} 
-#'   \item{uk_col_label} The original UK column labels.
+#'   \item{uk_col_label}{The original UK column labels.}
 #'   \item{eu_prod_na}{Equivalence to the EU prod_na variable, can be used for grouping. }
 #'   \item{row_oder}{Numeric ordeirng of the rows.}
 #'   \item{col_order}{Numeric ordering of the columns.}
