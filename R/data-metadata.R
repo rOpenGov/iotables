dd109b70a2b9ed330808ba7de0b0775335586882
antaldaniel <antaldaniel@gmail.com>
2018-12-11 11:16:55 +0100

    metadata tiding up and documentation

--- R/data-metadata.R
+++ R/data-metadata.R
@@ -3,7 +3,7 @@
 #' An arrangement of the Eurostat national accounts vocabulary, used to correctly
 #' order wide format rows and columns from bulk long-form tables. 
 #' @usage data(metadata)
-#' @format A data frame with 10 variables. #' 
+#' @format A data frame with 8 variables.
 #'\describe{
 #'   \item{variable}{Eurostat vocabulary source, i.e. t_rows, t_cols, prod_na, induse}
 #'   \item{group}{ Informal labelling for macroeconomic groups }
@@ -11,8 +11,6 @@
 #'   \item{label}{Eurostat label descriptions}
 #'   \item{quadrant}{Where to place the data from a long-form raw data file}  
 #'   \item{account_group}{Different from Eurostat tables, in thousand national currency units.}
-#'   \item{digit_1}{ third digit for ordering}
-#'   \item{digit_2}{ fourth digit for ordering}
 #'   \item{numeric_label}{ ordering from quadrant, account_group, digit_1, digit_2}     
 #'   \item{iotables_label}{Custom, machine_readable snake format variable names}                     
 #' }
---
+++ R/data-metadata_uk_2010.R
@@ -0,0 +1,20 @@
+#' Metadata for the uk_2010 dataset
+#'
+#' A vocabulary matching and ordering help for the Excel-imported UK data.
+#' @usage data(metadata_uk_2010)
+#' @format A data frame with 10 variables.
+#'\describe{
+#'   \item{variable}{Eurostat vocabulary source, i.e. t_rows, t_cols, prod_na, induse}
+#'   \item{uk_row} The UK row identifier. All '.' and ' & ' converted to '-'
+#'   \item{uk_row_label} The original UK row labels.
+#'   \item{uk_col} The UK row identifier. All '.' and ' & ' converted to '-'
+#'   \item{uk_col_label} The original UK column labels.
+#'   \item{eu_prod_na}{Equivalence to the EU prod_na variable, can be used for grouping. }
+#'   \item{row_oder}{Numeric ordeirng of the rows.}
+#'   \item{col_order}{Numeric ordering of the columns.}
+#'   \item{prod_na}{Eurostat label equivalents for rows}
+#'   \item{indue}{Eurostat label equivalents for columns}
+#' }
+#' @keywords data, datasets, input-output table, metadata, vocabulary, UK
+
+"metadata_uk_2010"
