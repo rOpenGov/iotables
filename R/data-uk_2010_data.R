3fe2d14bcc13f5b9a5db663a6b193ea9b787c503
antaldaniel <antaldaniel@gmail.com>
2018-12-11 23:02:54 +0100

    adding new data

---
+++ R/data-uk_2010_data.R
@@ -0,0 +1,20 @@
+#' United Kingdom Input-Output Analytical Tables, 2010												
+#'
+#' The Excel-imported UK data.
+#' @usage data(uk_2010_data)
+#' @format A data frame with 10 variables.
+#'\describe{
+#'   \item{uk_row}{The UK row identifier. Dots and '&' converted to '-'.} 
+#'   \item{uk_row_label}{The original UK row labels.}
+#'   \item{uk_col}{The UK row identifier. Dots and '&' converted to '-'.} 
+#'   \item{uk_col_label}{The original UK column labels.}
+#'   \item{geo}{Eurostat-style geocode, i.e. UK }
+#'   \item{geo_lab}{United Kingdom}
+#'   \item{indicator}{The name of the indicator, i.e. Excel sheet.}
+#'   \item{unit}{Eurostat label equivalents units, i.e. MIO_NAC.}
+#'   \item{unit_lab}{Eurostat label equivalents, i.e. millions of national currency unit.}
+#'   \item{values}{The numeric values of the variable}
+#' }
+#' @keywords data, datasets, input-output table, metadata, vocabulary, UK
+
+"uk_2010_data"
