1ba70df1cbb3529b6457334b6b206f21aabae7cd
antaldaniel <antaldaniel@gmail.com>
2018-12-22 09:37:59 +0100

    internal function, no example is needed

--- R/matrix_round.R
+++ R/matrix_round.R
@@ -5,8 +5,6 @@
 #' @param digits An integer number, defaults to \code{0}.
 #' @return The matrix, with the intact key column and the numeric columns rounded.
 #' @importFrom dplyr mutate_if
-#' @examples 
-#' matrix_round( iotable_get ( source = 'germany_1990') )
 
 matrix_round <- function(data_table, digits = 0) {
   
