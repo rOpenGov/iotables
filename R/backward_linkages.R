07d8397139d3ee98f7419ef57711b5a07d23ea0e
Daniel Antal <antaldaniel@users.noreply.github.com>
2020-05-18 18:55:02 +0200

    @return

--- R/backward_linkages.R
+++ R/backward_linkages.R
@@ -1,16 +1,18 @@
 #' Backward linkages
 #' 
-#' Backward linkages as defined by the Eurostat Manual of Supply, Use and Input-Output
-#' Tables (see p506-507.)
+#' Backward linkages as defined by the Eurostat Manual of Supply, Use and
+#' Input-Output Tables (see p506-507.)
 #' @param Im A Leontieff inverse matrix created by the
 #' \code{\link{leontieff_inverse_create}} function. 
+#' @return The vector of industry (product) backward linkages in a wide
+#' data.frame class, following the column names of the Leontieff 
+#' inverse matrix. 
 #' @importFrom dplyr mutate_if
 #' @examples 
-#' de_coeff <- input_coefficient_matrix_create( iotable_get(), digits = 4)
-#' 
+#' de_coeff <- input_coefficient_matrix_create( iotable_get(), 
+#'                                              digits = 4 )
 #' I <- leontieff_inverse_create (de_coeff)
-#' backward_linkages ( I )
-#' 
+#' backward_linkages (I)
 #' @export 
 
 backward_linkages <- function ( Im ) {
