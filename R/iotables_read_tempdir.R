5d91bfeeda2d7d650e2b06b7ed2a1411683faec7
Daniel Antal <antaldaniel@users.noreply.github.com>
2020-05-18 15:15:33 +0200

    new function itables_read_tempdir()

---
+++ R/iotables_read_tempdir.R
@@ -0,0 +1,36 @@
+#' Read input-output Tables from Temporary Directory
+#'
+#' Validate the \code{source} input parameter and try to load the table
+#' from the current sessions' temporary directory.
+#'  \itemize{
+#'  \item{\code{naio_10_cp1700}}{ Symmetric input-output table at basic prices (product by product)}
+#'  \item{\code{naio_10_pyp1700}}{ Symmetric input-output table at basic prices (product by product) (previous years prices)}
+#'  \item{\code{naio_10_cp1750}}{ Symmetric input-output table at basic prices (industry by industry)}
+#'  \item{\code{naio_10_pyp1750}}{ Symmetric input-output table at basic prices (industry by industry) (previous years prices) }
+#'  \item{\code{naio_10_cp15}}{ Supply table at basic prices incl. transformation into purchasers' prices }
+#'  \item{\code{naio_10_cp16}}{ Use table at purchasers' prices }
+#'  \item{\code{naio_10_cp1610}}{ Use table at basic prices }
+#'  \item{\code{naio_10_pyp1610}}{ Use table at basic prices (previous years prices) (naio_10_pyp1610) }
+#'  \item{\code{naio_10_cp1620}}{ Table of trade and transport margins at basic prices}
+#'  \item{\code{naio_10_pyp1620}}{ Table of trade and transport margins at previous years' prices}
+#'  \item{\code{naio_10_cp1630}}{ Table of taxes less subsidies on products at basic prices}
+#'  \item{\code{naio_10_pyp1630}}{Table of taxes less subsidies on products at previous years' prices}
+#'  \item{\code{uk_2010_siot}}{United Kingdom Input-Output Analytcal Tables data}
+#' } 
+#' @param source See the available list of sources above in the Description. 
+#' @return A nested data frame. Each input-output table is in a separate 
+#' row of the nested output, where all the metadata are in columns, and the
+#' actual, tidy, ordered input-output table is in the data \code{data} column.
+#' @examples
+#' iotables_read_tempdir ( source = "naio_10_cp1700" )
+#' @export
+
+iotables_read_tempdir <- function(source) {
+  validate_source(source)
+  temporary_file <- file.path(tempdir(), paste0(source, '.rds'))
+  if (file.exists(temporary_file)) {
+    readRDS( temporary_file)
+  }
+}
+
+                   
