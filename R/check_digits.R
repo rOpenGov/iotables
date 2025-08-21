9daf723d7897640a2a747b31c7c20a3cddd4f09f
Daniel Antal <antaldaniel@gmail.>
2025-08-21 15:41:04 +0200

    Checking if digit rounding does not cause problems.

--- R/check_digits.R
+++ R/check_digits.R
@@ -3,14 +3,16 @@
 #' @param digits Digit input to check for validity.
 #' @return An error if the digits are not \code{NULL} or an integer value.
 #' @keywords internal
-
 check_digits <- function(digits) {
-  
-  if (!is.null(digits)) { # Rounding digits must be numeric, if given
-    if ( ! inherits(digits, "numeric") ) {
-      stop ("Error in check_digits(digits): rounding 'digits' are not given as a numeric input.") }
+  if (!is.null(digits)) {
+    if (!inherits(digits, "numeric")) {
+      stop(
+        paste(
+          "Error in check_digits(digits):",
+          "rounding 'digits' must be numeric or NULL."
+        )
+      )
+    }
   }
+  invisible(TRUE)
 }
-
-
-
