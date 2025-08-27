83037bf2d2091c15617b482470cea2e241d4271b
Daniel Antal <antaldaniel@gmail.>
2025-08-27 23:30:03 +0200

    create test example data files

--- data-raw/create_germany_2009.R
+++ data-raw/create_germany_2009.R
@@ -1,15 +1,15 @@
-
 germany_2009 <- read.csv(file.path("data-raw", "UN_Manual", "germany_2009.csv"), stringsAsFactors = F)
 
 
-names ( germany_2009)
+names(germany_2009)
 
 germany_2009 %>%
-  set_names( c("prod_na", "prod_na_lab", "numeric_label", "quadrant", "iotables_row", 
-               "agriculture_group", "industry_group", "construction", "trade_group", "business_services_group", 
-               "other_services_group", "total", "final_consumption_households"))
+  set_names(c(
+    "prod_na", "prod_na_lab", "numeric_label", "quadrant", "iotables_row",
+    "agriculture_group", "industry_group", "construction", "trade_group", "business_services_group",
+    "other_services_group", "total", "final_consumption_households"
+  ))
 
 
- 
+
 germany_1995
-
--- data-raw/create_netherlands_example.R
+++ data-raw/create_netherlands_example.R
@@ -1,12 +1,15 @@
-library (dplyr) ; library(tidyr) ; library (devtools)
+library(dplyr)
+library(tidyr)
+library(devtools)
 
 read.csv(here::here("data-raw", "netherlands_2006.csv"))
-netherlands_2006  <- read.csv2 ( "data-raw/netherlands_2006.csv") 
+netherlands_2006 <- read.csv2("data-raw/netherlands_2006.csv")
 
-netherlands_2006  <- convert_to_ascii(netherlands_2006)
+netherlands_2006 <- convert_to_ascii(netherlands_2006)
 
-usethis::use_data ( netherlands_2006, 
-                    overwrite = TRUE)
+usethis::use_data(netherlands_2006,
+  overwrite = TRUE
+)
 
 
-load ( file.path( 'data', 'netherlands_2006.rda'))
+load(file.path("data", "netherlands_2006.rda"))
