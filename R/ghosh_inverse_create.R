07cf02208b384ea568f918b9497826c77b84e9bb
Daniel Antal <antaldaniel@gmail.>
2025-08-28 00:24:39 +0200

    code styling

--- R/ghosh_inverse_create.R
+++ R/ghosh_inverse_create.R
@@ -3,7 +3,7 @@
 #' Create the Ghosh inverse from output coefficients.
 #'
 #' @details
-#' The Ghosh inverse is defined as \deqn{G = (I - B)^{-1}}, 
+#' The Ghosh inverse is defined as \deqn{G = (I - B)^{-1}},
 #' where \eqn{B} is the output‑coefficient matrix created by
 #' [output_coefficient_matrix_create()].
 #' See the United Nations
@@ -32,7 +32,6 @@
 
 ghosh_inverse_create <- function(output_coefficients_matrix,
                                  digits = NULL) {
-  
   # The Ghosh-model is a dual pair of the Leontief-model, so we can use
   # the same functions, but with different inputs (output coefficients
   # instead of input coefficients.)
@@ -40,27 +39,30 @@ ghosh_inverse_create <- function(output_coefficients_matrix,
   ghosh_matrix <- leontief_matrix_create(
     technology_coefficients_matrix = output_coefficients_matrix
   )
-  
+
   Gm <- as.matrix(ghosh_matrix[, 2:ncol(ghosh_matrix)])
-  
+
   inverse <- solve(Gm)
-  
+
   if (sum(vapply(inverse, function(x) sum(is.nan(x)), numeric(1))) > 0) {
     stop("Error: Could not invert the Ghosh-matrix.")
   }
-  
+
   named_inverse <- cbind(
     as.data.frame(ghosh_matrix[, 1]),
     as.data.frame(inverse)
   ) %>%
-    dplyr::mutate(dplyr::across(where(is.factor), 
-                                function(x) as.character(x))
-                  )
-  
+    dplyr::mutate(dplyr::across(
+      where(is.factor),
+      function(x) as.character(x)
+    ))
+
   names(named_inverse) <- names(ghosh_matrix)
   row.names(named_inverse) <- seq_len(nrow(named_inverse))
-  
-  if (is.null(digits)) return(named_inverse)
-  
+
+  if (is.null(digits)) {
+    return(named_inverse)
+  }
+
   round_table(named_inverse, digits = digits)
 }
