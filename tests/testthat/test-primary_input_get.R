70961137cef5be382dc225d644f03fa2f1a4b2dc
Daniel Antal <antaldaniel@gmail.>
2025-08-31 19:25:42 +0200

    testthat correction

--- tests/testthat/test-primary_input_get.R
+++ tests/testthat/test-primary_input_get.R
@@ -1,5 +1,3 @@
-
-
 test_that("Retreieving a primary input vector with primary_input_get()", {
   siot <- iotable_get(
     source = "germany_1995",
@@ -7,7 +5,7 @@ test_that("Retreieving a primary input vector with primary_input_get()", {
     unit = "MIO_EUR",
     labelling = "short"
   )
-  
+
   expect_equal(as.character(unlist(primary_input_get(
     data_table = siot,
     primary_input = "D1"
@@ -16,14 +14,21 @@ test_that("Retreieving a primary input vector with primary_input_get()", {
 
 
 test_that("primary_input_get errors nicely on bad inputs", {
-  siot <- iotable_get(source = "germany_1995", geo = "DE", year = 1990,
-                      unit = "MIO_EUR", labelling = "short")
-  
-  expect_error(primary_input_get(NULL, "D1"),
-               "No input-output table")
-  expect_error(primary_input_get(siot, "not_a_label"),
-               "not found")
-  expect_error(primary_input_get(siot, c("D1", "D2")),
-               "single character")
-})
+  siot <- iotable_get(
+    source = "germany_1995", geo = "DE", year = 1990,
+    unit = "MIO_EUR", labelling = "short"
+  )
 
+  expect_error(
+    primary_input_get(NULL, "D1"),
+    "No input-output table"
+  )
+  expect_error(
+    primary_input_get(siot, "not_a_label"),
+    "not found"
+  )
+  expect_error(
+    primary_input_get(siot, c("D1", "D2")),
+    "single character"
+  )
+})
