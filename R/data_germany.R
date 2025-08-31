b86c016d8b8bc5f1f6439d1865cb3935bf7f6247
Daniel Antal <antaldaniel@gmail.>
2025-08-31 19:23:29 +0200

    roxygen markup error fix

--- R/data_germany.R
+++ R/data_germany.R
@@ -1,8 +1,8 @@
-#' @title Simple input-output table for Germany, 1995
+#' @title Simple input–output table for Germany, 1995
 #'
 #' @description
-#' Replication dataset from the **Eurostat Manual**, Table 15.4:
-#' *Input-output table of domestic output at basic prices (Version A)*.
+#' Replication dataset from the Eurostat Manual, Table 15.4:
+#' *Input–output table of domestic output at basic prices (Version A)*.
 #'
 #' @details
 #' This dataset is used for testing and documentation purposes. It is a
@@ -13,22 +13,21 @@
 #' The labels were slightly adjusted to reflect the transition from the
 #' ESA95 vocabulary to ESA2010 since the publication of the Manual.
 #'
-#' @source
-#' [Eurostat Manual of Supply, Use and Input-Output Tables, p. 482](
-#' https://ec.europa.eu/eurostat/documents/3859598/5902113/KS-RA-07-013-EN.PDF/
-#' b0b3d71e-3930-4442-94be-70b36cea9b39?version=1.0)
+#' @source Eurostat (2008). *Eurostat Manual of Supply, Use and Input–Output
+#' Tables*, p. 482. Available from:
+#' <https://ec.europa.eu/eurostat/documents/3859598/5902113/KS-RA-07-013-EN.PDF>
 #'
 #' @format A data frame with 228 observations and 11 variables:
 #' \describe{
 #'   \item{prod_na}{Technology codes in row names, following Eurostat
 #'     conventions.}
-#'   \item{prod_na_lab}{Longer labels corresponding to `t_rows2`.}
+#'   \item{prod_na_lab}{Longer labels corresponding to the `t_rows2` codes.}
 #'   \item{induse}{Column labels, following Eurostat conventions with
 #'     minor differences.}
-#'   \item{iotables_row}{Row labels (to be used in key columns), using
-#'     `iotables` abbreviations.}
-#'   \item{iotables_col}{Column labels using `iotables` abbreviations.}
-#'   \item{values}{Table values in million euros.}
+#'   \item{iotables_row}{Row labels (used in key columns) in `iotables`
+#'     abbreviations.}
+#'   \item{iotables_col}{Column labels in `iotables` abbreviations.}
+#'   \item{values}{Table values in millions of euros.}
 #'   \item{unit}{Unit identifier. Always `"MIO_EUR"`.}
 #'   \item{unit_lab}{Label for the unit: "million euros". Eurostat usually
 #'     provides both euro and national currency values.}
