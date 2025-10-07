7adab26087ddcdb55b550d1f230dc12b5a097a41
Daniel Antal <antaldaniel@gmail.>
2025-10-07 09:36:46 +0200

    prd_ava dictionary checked

--- data-raw/prd_ava.R
+++ data-raw/prd_ava.R
@@ -1,7 +1,19 @@
 library(dplyr)
+
+eurostat_voc <- readxl::read_excel(
+  here::here(
+    "data-raw",
+    "eurostat_vocabularies_2025.xlsx"
+  ),
+  sheet = "prd_ava"
+)
+
 prd_ava <- readxl::read_excel(here::here("data-raw", "prd_ava.xlsx")) %>%
   relocate(block, .before = uri) %>%
   arrange(numeric_order)
+
+assertthat::assert_that(length(setdiff(eurostat_voc$Id, prd_ava$id)) == 0)
+
 
 usethis::use_data(prd_ava)
 
