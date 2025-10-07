7e4fbcb81b8cc9898e0f0e176db4b1e4a23ed667
Daniel Antal <antaldaniel@gmail.>
2025-10-07 09:48:57 +0200

    fix prd_use

--- data-raw/prd_use.R
+++ data-raw/prd_use.R
@@ -1,8 +1,19 @@
-prd_use <- readxl::read_excel(here::here("data-raw", "prd_use.xlsx")) %>%
-  mutate(uri = sprintf("https://dd.eionet.europa.eu/vocabularyconcept/eurostat/prd_use/%s", notation)) %>%
+library(dplyr)
+
+eurostat_voc <- readxl::read_excel(here::here("data-raw", "eurostat_vocabularies_2025.xlsx"),
+  sheet = "prd_use"
+)
+
+prd_use <- readxl::read_excel(
+  here::here("data-raw", "prd_use.xlsx")
+) %>%
+  mutate(
+    uri = sprintf("https://dd.eionet.europa.eu/vocabularyconcept/eurostat/prd_use/%s", notation)
+  ) %>%
   arrange(numeric_order) %>%
   select(-ind_use_notation) %>%
   mutate(group = ifelse(grepl("^CPA_", notation), "Industry", group))
 
+assertthat::assert_that(length(setdiff(eurostat_voc$Id, prd_use$id)) == 0)
+
 usethis::use_data(prd_use)
-names(prd_use)
